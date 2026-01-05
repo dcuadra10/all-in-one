a8e8f560321c1503833c162d659e176f1bf99055
dcuadra10 <djpcuadra10@gmail.com>
2026-01-05 15:47:43 -0600

    fix: remove potential invisible char in syntax at line 1655

--- bot.js
+++ bot.js
@@ -1652,7 +1652,6 @@ client.on('interactionCreate', async interaction => {
 
     await interaction.showModal(modal);
   }
-
 } else if (interaction.isStringSelectMenu()) {
   if (interaction.customId === 'shop_buy_select') {
     const itemId = interaction.values[0];
